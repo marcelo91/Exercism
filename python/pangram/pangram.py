dfb0bd08987a545ef8dcac375580e29b106e3c73
Marcelo91 <marcelocotia91@gmail.com>
2023-12-21 08:48:50 -0300

    add pangram exercise

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,8 +1,2 @@
 def is_pangram(sentence):
-    alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    if alpha in sentence:
-        return True
-    else:
-        return False
-    
-print(is_pangram('ola meu chacho'))
+    return len([letter for letter in set(sentence.lower()) if letter.isalpha()]) == 26
